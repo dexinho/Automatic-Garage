2dfea08d6b0737bb99fd89691e477c5825d13c68
Jasmin Hidanovic <130430763+dexinho@users.noreply.github.com>
2023-09-24 23:38:09 +0200

    made garage dynamic

--- script.js
+++ script.js
@@ -1,119 +1,204 @@
 class Vehicle {
-    constructor({type, registrationNumber, model, brand, numberOfWheels }) {
-        // this.type = type
-        // this.registrationNumber = registrationNumber
-        // this.model = model
-        // this.brand = brand
-        // this.numberOfWheels = numberOfWheels
-        for (const key in arguments[0])
-            this[key] = arguments[0][key]
+    constructor({ registration, model, brand, numberOfWheels }) {
+        this.registration = registration
+        this.model = model
+        this.brand = brand
+        this.numberOfWheels = numberOfWheels
     }
 }
 
 class Car extends Vehicle {
-    constructor(characteristics) {
-        super(characteristics)
+    constructor(carProperties) {
+        super(carProperties)
     }
 }
 
 class Bike extends Vehicle {
-    constructor(characteristics) {
-        super(characteristics)
+    constructor(bikeProperties) {
+        super(bikeProperties)
     }
 }
 
 class Truck extends Vehicle {
-    constructor(characteristics) {
-        super(characteristics)
+    constructor(truckProperties) {
+        super(truckProperties)
+    }
+}
+
+class Helikopter extends Vehicle {
+    constructor(heliProperties) {
+        super(heliProperties)
     }
 }
 
 class Garage {
-    #parkedCars = []
-    #parkedBikes = []
-    #parkedTrucks = []
+    constructor(id, parkingSpacesPerVehicle) {
 
-    constructor({id, carParkingSpaces, bikeParkingSpaces, truckParkingSpaces }) {
-        // this.id = id
-        // this.carParkingSpaces = carParkingSpaces
-        // this.bikeParkingSpaces = bikeParkingSpaces
-        // this.truckParkingSpaces = truckParkingSpaces
+        this.allVehiclesParked = this.loadFromLocalStorage()
 
-        this.parkedVehicles = JSON.parse(localStorage.getItem(id)) || []
-        for (const key in arguments[0]) {
-            this[key] = arguments[0][key]
+        for (const key in parkingSpacesPerVehicle) {
+            this[key] = parkingSpacesPerVehicle[key]
+            if (Object.keys(this.allVehiclesParked).length < Object.keys(parkingSpacesPerVehicle).length)
+                this.allVehiclesParked[key] = []
         }
+
+        this.id = id
+        // this.freezeObject()
+    }
+
+    loadFromLocalStorage() {
+        return JSON.parse(localStorage.getItem(this.id)) || {}
+    }
+
+    saveToLocalStorage = () => {
+        localStorage.setItem(this.id, JSON.stringify(this.allVehiclesParked))
     }
 
     add = (vehicle) => {
-        if (vehicle instanceof Car && this.#parkedCars.length <= this.carParkingSpaces)
-            this.#parkedCars.push(vehicle)
-        else if (vehicle instanceof Bike && this.#parkedBikes.length <= this.carParkingSpaces)
-            this.#parkedCars.push(vehicle)
-        else if (vehicle instanceof Truck && this.#parkedTrucks.length <= this.carParkingSpaces)
-            this.#parkedCars.push(vehicle)
-        else console.log("There is no parking space")
+        const vehicleType = vehicle.constructor.name.toLowerCase()
+        const allVehiclesParked = this.allVehiclesParked // da li se moze vani deklarisati da se ne bi svaki put deklarisala unutar metoda
+
+        if (allVehiclesParked[vehicleType].length <= this[vehicleType]
+            && allVehiclesParked[vehicleType].every(el => el.registration !== vehicle.registration)) {
+            allVehiclesParked[vehicleType].push(vehicle)
+            console.log(`${vehicleType[0].toUpperCase() + vehicleType.slice(1)} is parked with registration number: ${vehicle.registration}`)
+        }
+        else console.log(`There is no more parking space for ${vehicleType}s`)
+
+
+        this.saveToLocalStorage()
     }
+
 
     remove = (vehicle) => {
-        if (vehicle instanceof Car) {
-            this.#parkedCars = this.#parkedCars
-            .filter(machine => machine.registrationNumber !== vehicle.registrationNumber)
-        }            
-        else if (vehicle instanceof Bike) {
-            this.#parkedBikes = this.#parkedBikes
-            .filter(machine => machine.registrationNumber !== vehicle.registrationNumber)
-        } 
-        else if (vehicle instanceof Truck) {
-            this.#parkedTrucks = this.#parkedTrucks
-            .filter(machine => machine.registrationNumber !== vehicle.registrationNumber)
-        }
+        const vehicleType = vehicle.constructor.name.toLowerCase()
+        const allVehiclesParked = this.allVehiclesParked
+
+        allVehiclesParked[vehicleType] = allVehiclesParked[vehicleType].filter(el => {
+            if (el.registration === vehicle.registration) {
+                console.log(`${vehicle.constructor.name} is removed with registration number: ${vehicle.registration}`)
+                return false
+            }
+            return true
+        })
+
+        this.saveToLocalStorage()
     }
 
-    findVehicleByRegistrationNumber = (number) => {
-        return this.#parkedCars.find(vehicle => number === vehicle.registrationNumber)
-        ?? this.#parkedBikes.find(vehicle => number === vehicle.registrationNumber)
-        ?? this.#parkedTrucks.find(vehicle => number === vehicle.registrationNumber)
-        ?? `There is no vehicle with ${number} registration number parked in the garage!`
+    findVehicleByRegistrationNumber = (registrationNumber) => {
+        const allVehiclesParked = this.allVehiclesParked
+        for (const vehicleType in allVehiclesParked) {
+            if (allVehiclesParked[vehicleType].find(vehicle => vehicle.registration === registrationNumber))
+                return JSON.stringify(allVehiclesParked[vehicleType].find(vehicle => vehicle.registration === registrationNumber),
+                    (key, value) => {
+                        return key === '' ? { type: vehicleType, ...value } : value
+                    }, 2)
+        }
+
+        return `There is no vehicle parked with ${registrationNumber} registration number`
     }
 
     showAllVehiclesParked = () => {
-
+        const allVehiclesParked = this.allVehiclesParked
+        for (const vehicleType in allVehiclesParked) {
+            allVehiclesParked[vehicleType].forEach(vehicle => {
+                console.log(JSON.stringify(vehicle,
+                    (key, value) => {
+                        return key === '' ? { type: vehicleType, ...value } : value
+                    }, 2))
+            })
+        }
     }
 
-    freeParkingSpaces = () => {
+    freezeObject() {
+        // for (const key in this) {
+        //     Object.defineProperties(this, {
+        //         [key]: {
+        //             configurable: false,
+        //             writable: false,
+        //             enumerable: false,
+        //         },
+        //     })
+        // }
+        Object.freeze(this)
+    }
 
+    showFreeParkingSpaces = () => {
+        const allVehiclesParked = this.allVehiclesParked
+        for (const vehicleType in allVehiclesParked) {
+            console.log(`${vehicleType}: ${this[vehicleType] - allVehiclesParked[vehicleType].length}`)
+        }
+    }
+
+    set updateGarageSlots(newValues) {
+
+        const allVehiclesParked = this.allVehiclesParked
+
+        for (const vehicleType in allVehiclesParked) {
+            delete this[vehicleType]
+            if (!newValues.hasOwnProperty(vehicleType)) delete allVehiclesParked[vehicleType]
+        }
+
+        for (const key in newValues) {
+            this[key] = newValues[key]
+        }
     }
 }
 
-const bigGarage = new Garage({
-    id: 1,
-    carParkingSpaces: 50,
-    bikeParkingSpaces: 20,
-    truckParkingSpaces: 10
+const bigGarage = new Garage(111, {
+    bike: 10,
+    car: 10,
+    truck: 5,
 })
 
 const car1 = new Car({
-    type: 'car',
-    registrationNumber: 1251521431232,
-    model: 'sedan',
-    brand: 'x5',
-    numberOfWheels: 4,
+    registration: 123124124124,
+    model: '22',
+    brand: '2111',
+    numberOfWheels: 20,
 })
 
 const car2 = new Car({
-    type: 'car',
-    registrationNumber: 8512051215512,
-    model: 'sedan',
-    brand: 'x7',
-    numberOfWheels: 4,
+    registration: 13224124,
+    model: 'aa',
+    brand: 's',
+    numberOfWheels: 20,
 })
+
+const truck1 = new Truck({
+    registration: 124124,
+    model: 'BB',
+    brand: 's',
+    numberOfWheels: 20,
+})
+
+const bike1 = new Bike({
+    registration: 6624,
+    model: '2aa',
+    brand: 's',
+    numberOfWheels: 20,
+})
+
 
 bigGarage.add(car1)
 bigGarage.add(car2)
+bigGarage.add(truck1)
+bigGarage.add(bike1)
 
 bigGarage.remove(car2)
 
-console.log('GARAZA', bigGarage)
-console.log(bigGarage.findVehicleByRegistrationNumber(1251521431232))
-// console.log(bigGarage)
+// bigGarage.showAllVehiclesParked()
+
+bigGarage.updateGarageSlots = {
+    truck: 0
+}
+
+bigGarage.showAllVehiclesParked()
+
+// bigGarage.updateGarageSlots = {
+//     helikopter: 44,
+//     car: 55,
+//     truck: 66,
+// }
+
+// bigGarage.showFreeParkingSpaces()
